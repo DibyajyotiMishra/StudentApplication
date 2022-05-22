a26dd2edc61fca5fc2ec48fb490de8466a0bd6ba
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2022-05-22 08:08:08 +0530

    feat:  add PUT and DELETE

--- src/main/java/com/dibyajyoti/student/api/StudentService.java
+++ src/main/java/com/dibyajyoti/student/api/StudentService.java
@@ -1,6 +1,10 @@
 package com.dibyajyoti.student.api;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +20,38 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> existingStudent = studentRepository.findStudentByEmail(student.getEmail());
+        if (existingStudent.isPresent()) {
+            throw new IllegalStateException("Student with email " + student.getEmail() + " already exists");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id) {
+        boolean studentExists = studentRepository.existsById(id);
+        if (!studentExists) {
+            throw new IllegalStateException("Student with id " + id + " does not exist");
+        }
+        studentRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateStudent(Long id, String name, String email) {
+        Student existingStudent = studentRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalStateException("Student with id " + id + " does not exist"));
+        if (name != null && name.length() > 2 && !Objects.equals(name, existingStudent.getName())) {
+            existingStudent.setName(name);
+        }
+        if (email != null && email.length() > 2 && !Objects.equals(email, existingStudent.getEmail())) {
+            boolean studentExists = studentRepository.existsById(id);
+            if (!studentExists) {
+                throw new IllegalStateException("Student with id " + id + " does not exist");
+            }
+            existingStudent.setEmail(email);
+        }
     }
 }
